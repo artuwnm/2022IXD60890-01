e26129a99a98a43cc3721e6698b7fd16518938ea
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-10-24 14:01:27 +0800

    module 7

---
+++ chiang.eileen/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Product List</title>
+	<?php include "parts/meta.php"; ?>
+</head>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+	
+	<div class="container" id="#typography">
+    	
+    	<div class="card soft" id="paragraphs">
+    		<h3>Welcome to Something Vintage!</h3>
+    		<p>Lorem ipsum dolor, sit amet consectetur, adipisicing elit. Quia explicabo quod alias illum fuga aliquid voluptate saepe architecto, maiores iste harum cumque nihil, vitae quidem error amet sapiente distinctio vel?</p>
+    		<p>Optio reprehenderit expedita corporis ipsam quos illum iste velit debitis quas cum. Impedit, dolores quisquam asperiores totam voluptate quia unde iusto, similique quae enim. Doloribus nemo esse provident dicta iure.</p>
+    		<p>Iste ut tempore amet doloribus modi. Perferendis in reprehenderit tempore soluta minus labore autem adipisci, eveniet natus, perspiciatis repellendus illo deleniti blanditiis, modi, eligendi! Possimus, sunt. Sunt animi itaque, exercitationem!</p>
+    	</div>
+    </div>
+
+    <div class="container" id="viewwindow">
+  
+        <div class="view-window" style="background-image: url(./img/vintage.jpeg);">
+        	
+        </div>
+
+  <h3>Featured Fashion of The Month</h3>
+
+        <div class="grid gap">
+          	<div class="col-xs-12 col-md-4">
+          		<figure class="figure product-overlay">
+        	       <img src="https://via.placeholder.com/400x400?text=product" alt="">
+        	       <figcaption>
+        	       	    <div class="caption-body">
+	                      <div>Product Name</div>
+	                      <div>$3.99</div>
+	                    </div>
+        	       </figcaption>
+                </figure>
+          	</div>
+          	<div class="col-xs-12 col-md-4">
+          		<figure class="figure product-overlay">
+        	       <img src="https://via.placeholder.com/400x400?text=product" alt="">
+        	       <figcaption>
+        	       	    <div class="caption-body">
+	                      <div>Product Name</div>
+	                      <div>$3.99</div>
+	                    </div>  
+        	       </figcaption>
+                </figure>
+          	</div>
+          	<div class="col-xs-12 col-md-4">
+          		<figure class="figure product-overlay">
+        	       <img src="https://via.placeholder.com/400x400?text=product" alt="">
+        	       <figcaption>
+        	       	    <div class="caption-body">
+	                      <div>Product Name</div>
+	                      <div>$3.99</div>
+	                    </div>  
+        	       </figcaption>
+                </figure>
+          	</div>
+        </div>
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ chiang.eileen/product_item.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Product Item</title>
+	<?php include "parts/meta.php"; ?>
+
+</head>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+	
+	<div class="container">
+		<div class="card soft">
+			<h2>Product Item</h2>
+
+			<p>This is item # <?= $_GET['id']  ?> </p>
+
+		</div>
+	</div>
+
+
+</body>
+</html>
