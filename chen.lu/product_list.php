7df14112fba5925d7f4ced3ad1610c6bbf96359e
LuChen <113313566+lchen70@users.noreply.github.com>
2022-10-24 01:15:27 -0700

    Add files via upload

---
+++ chen.lu/product_list.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Product List</title>
+	
+	<?php include "parts/meta.php"; ?>
+
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+	
+	<div class="container">
+		<div class="card soft">
+			<h2>Product List</h2>
+
+			<ul>
+				<li><a href="product_item.php?id=1">Product 1</a></li>
+				<li><a href="product_item.php?id=2">Product 2</a></li>
+				<li><a href="product_item.php?id=3">Product 3</a></li>
+				<li><a href="product_item.php?id=4">Product 4</a></li>
+			</ul>
+		</div>
+	</div>
+
+
+</body>
+</html>
