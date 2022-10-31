8123c75b86f0dbe68e83c964224bfae4f8cfbc4b
LuChen <113313566+lchen70@users.noreply.github.com>
2022-10-30 21:44:43 -0700

    Add files via upload

---
+++ chen.lu/parts/meta.php
@@ -0,0 +1,12 @@
+
+
+
+	<meta name="viewpoint" content="width=device-width">
+
+	<base href="http://lchen.co/IXD608/chen.lu/">
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+
+	<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
