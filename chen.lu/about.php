03a3396b2731fffdc10e91f87cbc18d388c710be
LuChen <113313566+lchen70@users.noreply.github.com>
2022-10-30 21:45:37 -0700

    Add files via upload

---
+++ chen.lu/about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About</title>
+
+	<?php include "parts/meta.php";?>
+</head>
+<body>
+    
+    <?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>About</h2>
+			<p>Place holder!</p>
+            
+		</div>
+	</div>
+	
+</body>
+</html>
---
+++ chen.lu/cart.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>cart</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+    
+    <?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Cart</h2>
+            <p>yeah</p>
+           
+		</div>
+	</div>
+	
+</body>
+</html>
