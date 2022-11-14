523f4ca815c7e2c1fb1548dce74803f24e16ba10
LuChen <113313566+lchen70@users.noreply.github.com>
2022-11-14 03:36:12 -0800

    Add files via upload

---
+++ chen.lu/lib/php/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+function MYSQLIAuth(){
+	return [
+		"localhost", //mysql host
+		"lchen_ixd608", //mysql user name
+		"iBWgm2&oZ", //mysql user password
+		"lchen_ixd608" //mysql database
+	];
+}
