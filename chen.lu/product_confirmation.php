893d976bf8bd74fcff654fc6abf663743ee06d7f
LuChen <113313566+lchen70@users.noreply.github.com>
2022-11-14 03:31:43 -0800

    Add files via upload

---
+++ chen.lu/product_confirmation.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Confirmation Page</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+    
+    <?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Thank you for your purchase</h2>
+			<p><a href="product_list.php">Continue Shopping</a></p>
+            
+           
+		</div>
+	</div>
+	
+</body>
+</html>
