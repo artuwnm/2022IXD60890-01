d4de10ea9e30506d1a4cb0d4c99904e6d76759a3
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-11-14 01:37:36 -0800

    module 10

--- chiang.eileen/product_list.php
+++ chiang.eileen/product_list.php
@@ -3,26 +3,43 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Product List</title>
+
 	<?php include "parts/meta.php"; ?>
 </head>
-</head>
 <body>
+    
+    <?php include "parts/navbar.php"; ?>
 
-	<?php include "parts/navbar.php"; ?>
-	
 	<div class="container">
 		<div class="card soft">
 			<h2>Product List</h2>
+            
 
-			<ul>
-				<li><a href="product_item.php?id=1">Product 1</a></li>
-				<li><a href="product_item.php?id=2">Product 2</a></li>
-				<li><a href="product_item.php?id=3">Product 3</a></li>
-				<li><a href="product_item.php?id=4">Product 4</a></li>
-			</ul>
+			<?php
+
+            include_once "lib/php/functions.php";
+            include_once "parts/templates.php";
+
+            $result = makeQuery(
+               makeConn(),
+               "
+               SELECT *
+               FROM `products`
+               ORDER BY `date_create` DESC
+               LIMIT 12
+               "
+            );
+
+            echo "<div class='productlist grid gap'>", array_reduce($result,'productListTemplate'),"</div>";
+
+			?>
 		</div>
 	</div>
-
-
+	
 </body>
 </html>
+
+
+
+
+
