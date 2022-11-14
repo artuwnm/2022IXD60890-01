714eef1d28a84537facc35dbf78897fddfe0ca7c
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-11-14 01:40:21 -0800

    module 10

--- chiang.eileen/parts/navbar.php
+++ chiang.eileen/parts/navbar.php
@@ -1,17 +1,17 @@
 
-<header class="navbar">
+	<header class="navbar">
 		<div class="container display-flex">
 			<div class="flex-none">
-				<h1>Store</h1>
+				<h1>Something Vintage</h1>
 			</div>
 			<div class="flex-stretch"></div>
 			<nav class="nav nav-flex flex-none">
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="product_list.php">Store</a></li>
-					<li><a href="#">Cart</a></li>
-				</ul>
+			   <ul>
+			      <li><a href="index.php">Home</a></li>
+			      <li><a href="product_list.php">Product</a></li>
+			      <li><a href="about.php">About</a></li>
+			      <li><a href="product_cart.php">Cart</a></li>
+			   </ul>
 			</nav>
-
 		</div>
 	</header>
