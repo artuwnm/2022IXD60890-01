05a844bad04f32c6a2f04c0dda0878589c4726aa
LuChen <113313566+lchen70@users.noreply.github.com>
2022-11-21 03:56:55 -0800

    Update product_added_to_cart.php

--- chen.lu/product_added_to_cart.php
+++ chen.lu/product_added_to_cart.php
@@ -4,6 +4,8 @@ include_once "lib/php/functions.php";
 
 $product = makeQuery (makeConn(), "SELECT * FROM `products` WHERE `id`=".$_GET['id'])[0];
 
+$cart_product = cartItemById($_GET['id']);
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,12 +21,13 @@ $product = makeQuery (makeConn(), "SELECT * FROM `products` WHERE `id`=".$_GET['
 	<div class="container">
 		<div class="card soft">
 			<h2>You added <?= $product->title ?> to your cart</h2>
-            
-            <div class="display-flex">
-               <div class="flex-none"><a href="product_list.php">Continue Shopping</a></div>
-               <div class="flex-stretch"></div>
-               <div class="flex-none"><a href="product_cart.php">Go to Cart</a></div>
-            </div>
+			<p>There are now <?= $cart_product->amount ?> of <?= $product->title ?> in your cart.</p>
+
+			<div class="display-flex">
+				<div class="flex-none"><a href="product_list.php">Continue Shopping</a></div>
+				<div class="flex-stretch"></div>
+				<div class="flex-none"><a href="product_cart.php">Go To Cart</a></div>
+			</div>
 		</div>
 	</div>
 
