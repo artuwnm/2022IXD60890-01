2697f647df1966caae789f30d5024dfe09058830
LuChen <113313566+lchen70@users.noreply.github.com>
2022-11-21 03:55:34 -0800

    Update product_item.php

--- chen.lu/product_item.php
+++ chen.lu/product_item.php
@@ -10,7 +10,7 @@ $image_elements = array_reduce($images,function($r,$o){
 	return $r."<img src='img/$o'>";
 });
 
-// print_p($product);
+//print_p($_SESSION);
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -39,16 +39,19 @@ $image_elements = array_reduce($images,function($r,$o){
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-5">
-				<div class="card soft flat">
+				<form class="card soft flat" method="post" action="cart_actions.php?action=add-to-cart">
+
+                    <input type="hidden" name="product-id" value="<?= $product->id ?>">
+
 					<div class="card-section">
-						<h2 class="product-title"><?= $product->title ?></h2>
+						<h2 class="product-title"><?= $product->name ?></h2>
 						<div class="product-price">&dollar;<?= $product->price ?></div>
 					</div>
 
 					<div class="card-section">
 						<label for="product-amount" class="form-label">Amount</label>
-						<div class="form-select" id="product-amount">
-							<select>
+						<div class="form-select" >
+							<select id="product-amount" name="product-amount">
 								<option>1</option>
 								<option>2</option>
 								<option>3</option>
@@ -64,13 +67,13 @@ $image_elements = array_reduce($images,function($r,$o){
 					</div>
 
 					<div class="card-section">
-						<a href="product_added_to_cart.php?id=<?= $product->id ?>" class="form-button">Add To Cart</a>
+						<input type="submit" class="form-button" value="Add To Cart">
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 
-		<div class="card soft light">
+		<div class="card soft white">
 			<p><?= $product->description ?></p>
 			
 		</div>
