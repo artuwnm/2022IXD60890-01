23f9427abfbd26f1f62f26d71b86c850704d5acd
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-11-20 16:13:57 -0800

    module 11

--- chiang.eileen/parts/navbar.php
+++ chiang.eileen/parts/navbar.php
@@ -1,17 +1,25 @@
+<?php
 
-	<header class="navbar">
-		<div class="container display-flex">
-			<div class="flex-none">
+include_once "lib/php/functions.php";
+
+?>
+
+<header class="navbar">
+	<div class="container display-flex">
+		<div class="flex-none">
 				<h1>Something Vintage</h1>
-			</div>
-			<div class="flex-stretch"></div>
-			<nav class="nav nav-flex flex-none">
-			   <ul>
-			      <li><a href="index.php">Home</a></li>
-			      <li><a href="product_list.php">Product</a></li>
-			      <li><a href="about.php">About</a></li>
-			      <li><a href="product_cart.php">Cart</a></li>
-			   </ul>
-			</nav>
 		</div>
-	</header>
+		<div class="flex-stretch"></div>
+		<nav class="nav nav-flex flex-none">
+			  <ul>
+			     <li><a href="index.php">Home</a></li>
+			     <li><a href="product_list.php">Product</a></li>
+			     <li><a href="about.php">About</a></li>
+			     <li><a href="product_cart.php">
+			        <span>Cart</span>
+			        <span class="badge"><?= makeCartBadge(); ?></span>
+			    </a></li>
+			  </ul>
+		</nav>
+	</div>
+</header>
