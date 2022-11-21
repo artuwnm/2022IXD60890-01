69c4771b7a2e0532a54087e5202a3f657b495572
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-11-20 16:13:02 -0800

    module 11

---
+++ chiang.eileen/lib/php/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+function MYSQLIAuth() {
+	return [
+		"localhost", // mysql host
+		"echiang_ixd608", // mysql username
+		"echiang_ixd608", //mysql user password
+		"echiang_ixd608" // mysql database name
+	];
+}
