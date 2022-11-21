2d2c57ac740a4dd0786674fd10ec061d94b7bf86
LuChen <113313566+lchen70@users.noreply.github.com>
2022-11-21 03:58:35 -0800

    Update templates.php

--- chen.lu/parts/templates.php
+++ chen.lu/parts/templates.php
@@ -2,36 +2,84 @@
 
 
 function productListTemplate($r,$o) {
-return $r.<<<HTML
+   return $r.<<<HTML
 <a class="col-xs-12 col-md-4" href="product_item.php?id=$o->id">
-    <figure class="figure product display-flex flex-column">
-        <div class="flex-stretch">
-            <img src="./img/$o->thumbnail"; alt="">
-        </div>
-        <figcaption class="flex-none">
-            <div>&dollar;$o->price</div>
-			<div>$o->name</div>
-        </figcaption>
-    </figure>
+   <figure class="figure product display-flex flex-column">
+      <div class="flex-stretch">
+         <img src="img/$o->thumbnail" alt="">
+      </div>
+      <figcaption class="flex-none">
+         <div>$o->name</div>
+         <div>&dollar;$o->price</div>
+      </figcaption>
+   </figure>   
 </a>
 HTML;
 }
 
 
+function selectAmount($amount=1,$total=10) {
+   $output = "<select name= 'amount'>";
+   for($i=1;$i<=$total;$i++){
+      $output .="<option ".($i==$amount?"selected":"").">$i</option>";
+   }
+   $output .= "</select>";
+   return $output;
+}
+
 function cartListTemplate($r,$o){
+$totalfixed = number_format($o->total,2,'.','');
+$selectamount = selectAmount($o->amount,10);
 return $r.<<<HTML
 <div class="display-flex">
-    <div class="flex-none images-thumbs">
-        <img src="./img/$o->thumbnail">
-    </div>
-    <div class="flex-stretch">
+   <div class="flex-none images-thumbs">
+        <img src="img/$o->thumbnail">
+   </div>
+   <div class="flex-stretch">
         <strong>$o->name</strong>
-        <div>Delete</div>
-        </div>
-        <div class="flex-none">
-        &dollar;$o->price
-    </div>
-    </div>
-HTML
+        <form action="cart_actions.php?action=delete-cart-item" method="post">
+           <input type="hidden" name="id" value="$o->id">
+           <input type="submit" class="form-button inline" value="Delete" style="font-size: 0.8em">
+        </form>
+   </div>
+   <div class="flex-none">
+        <div>&dollar;$totalfixed</div>
+        <form action="cart_actions.php?action=update-cart-item" method="post" onchange="this.submit()">
+           <input type="hidden" name="id" value="$o->id">
+           <div class="form-select" style="font-size: 0.8em">
+              $selectamount
+            </div>
+        </form>
+   </div>
+</div>
+HTML;
+}
 
-;}
+
+function cartTotals(){
+   $cart = getCartItems();
+
+   $cartprice = array_reduce($cart,function($r,$o){return $r + $o->total;},0);
+
+   $pricefixed = number_format($cartprice,2,'.','');
+   $taxfixed = number_format($cartprice*0.0725,2,'.','');
+   $taxedfixed = number_format($cartprice*1.0725,2,'.','');
+
+return <<<HTML
+<div class="card-section display-flex">
+   <div class="flex-stretch"><strong>Sub Total</strong></div>
+   <div class="flex-none">&dollar;$pricefixed</div>
+</div>
+<div class="card-section display-flex">
+   <div class="flex-stretch"><strong>Taxes</strong></div>
+   <div class="flex-none">&dollar;$taxfixed</div>
+</div>
+<div class="card-section display-flex">
+   <div class="flex-stretch"><strong>Total</strong></div>
+   <div class="flex-none">&dollar;$taxedfixed</div>
+</div>
+<div class="card-section">
+   <a href="product_checkout.php" class="form-button">Checkout</a>
+</div>
+HTML;
+}
