52dee5b2d280c808c27ce58af9456e277469b720
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-11-27 23:47:59 -0800

    module 12

--- chiang.eileen/index.php
+++ chiang.eileen/index.php
@@ -14,7 +14,7 @@
     	
     	<div class="card soft" id="paragraphs">
     		<h3>Welcome to Something Vintage!</h3>
-    		<p>Lorem ipsum dolor, sit amet consectetur, adipisicing elit. Quia explicabo quod alias illum fuga aliquid voluptate saepe architecto, maiores iste harum cumque nihil, vitae quidem error amet sapiente distinctio vel?</p>
+    		<p>Something Vintage is a home of vintage clothing collection. We are not just a typical ecommerce site for online shopping. We pay tribute to every piece of clothing from our history. Every clothing has its own story.</p>
     		<p>Optio reprehenderit expedita corporis ipsam quos illum iste velit debitis quas cum. Impedit, dolores quisquam asperiores totam voluptate quia unde iusto, similique quae enim. Doloribus nemo esse provident dicta iure.</p>
     		<p>Iste ut tempore amet doloribus modi. Perferendis in reprehenderit tempore soluta minus labore autem adipisci, eveniet natus, perspiciatis repellendus illo deleniti blanditiis, modi, eligendi! Possimus, sunt. Sunt animi itaque, exercitationem!</p>
     	</div>
@@ -22,47 +22,34 @@
 
     <div class="container" id="viewwindow">
   
-        <div class="view-window" style="background-image: url(./img/vintage.jpeg);">
+        <div class="view-window" style="background-image: url(./img/vintage.jpg);">
         	
         </div>
 
   <h3>Featured Fashion of The Month</h3>
 
-        <div class="grid gap">
-          	<div class="col-xs-12 col-md-4">
-          		<figure class="figure product-overlay">
-        	       <img src="https://via.placeholder.com/400x400?text=product" alt="">
-        	       <figcaption>
-        	       	    <div class="caption-body">
-	                      <div>Product Name</div>
-	                      <div>$3.99</div>
-	                    </div>
-        	       </figcaption>
-                </figure>
-          	</div>
-          	<div class="col-xs-12 col-md-4">
-          		<figure class="figure product-overlay">
-        	       <img src="https://via.placeholder.com/400x400?text=product" alt="">
-        	       <figcaption>
-        	       	    <div class="caption-body">
-	                      <div>Product Name</div>
-	                      <div>$3.99</div>
-	                    </div>  
-        	       </figcaption>
-                </figure>
-          	</div>
-          	<div class="col-xs-12 col-md-4">
-          		<figure class="figure product-overlay">
-        	       <img src="https://via.placeholder.com/400x400?text=product" alt="">
-        	       <figcaption>
-        	       	    <div class="caption-body">
-	                      <div>Product Name</div>
-	                      <div>$3.99</div>
-	                    </div>  
-        	       </figcaption>
-                </figure>
-          	</div>
-        </div>
+        <div class="container">
+    <div class="card soft">
+      <h2>Winter Collection</h2>
+
+      <?php
+
+      include_once "lib/php/functions.php";
+      include_once "parts/templates.php";
+
+      $result = makeQuery(
+        makeConn(),
+        "
+        SELECT *
+        FROM `products`
+        ORDER BY `date_create` 
+        LIMIT 6
+        "
+      );
+
+      echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
+
+      ?>
 
 
 
