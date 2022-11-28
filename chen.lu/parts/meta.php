e6ca558efe8e91bf4213ba91b48fd522cc145d7f
LuChen <113313566+lchen70@users.noreply.github.com>
2022-11-28 01:24:35 -0800

    Update meta.php

--- chen.lu/parts/meta.php
+++ chen.lu/parts/meta.php
@@ -1,12 +1,11 @@
 
 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<meta name="viewpoint" content="width=device-width">
-
-	<base href="http://lchen.co/IXD608/chen.lu/">
+	<base href="http://lchen.co/IXD_608/chen.lu/">
 
 	<link rel="stylesheet" href="lib/css/styleguide.css">
 	<link rel="stylesheet" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" href="css/storetheme.css">
 
-	<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
