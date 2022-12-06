dea45d8daf798f9c0c8fdeaeca0dd4403c359040
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-12-06 04:48:08 -0800

    Update auth.php

--- chiang.eileen/lib/php/auth.php
+++ chiang.eileen/lib/php/auth.php
@@ -8,3 +8,12 @@ function MYSQLIAuth() {
 		"echiang_ixd608" // mysql database name
 	];
 }
+
+function PDOAuth() {
+	return [
+		"mysql:host=localhost;dbname=echiang_ixd608", // host and database name
+		"echiang_ixd608", // mysql username
+		"echiang_ixd608", //mysql user password
+		[PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"]
+	];
+}
