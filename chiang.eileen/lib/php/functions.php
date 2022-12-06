33d2abd658a95de44993fdc069918a6539662040
EILEENCHIANGx <113303081+EILEENCHIANGx@users.noreply.github.com>
2022-12-06 04:48:30 -0800

    Update functions.php

--- chiang.eileen/lib/php/functions.php
+++ chiang.eileen/lib/php/functions.php
@@ -23,6 +23,15 @@ function makeConn() {
    return $conn;
 }
 
+function makePDOConn() {
+  try {
+    $conn = new PDO(...PDOAuth());
+  } catch (PDOException $e) {
+    die($e->getMessage());
+  }
+  return $conn;
+}
+
 
 function makeQuery($conn,$qry){
    $result = $conn->query($qry);
