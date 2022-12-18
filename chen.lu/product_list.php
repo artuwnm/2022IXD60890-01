8c4cbda43ed10ed10d72551578ee9b8e1974e642
LuChen <113313566+lchen70@users.noreply.github.com>
2022-12-18 14:57:57 -0800

    Update product_list.php

--- chen.lu/product_list.php
+++ chen.lu/product_list.php
@@ -31,7 +31,7 @@ include_once "parts/templates.php";
 
 	<div class="form-control">
 		<div class="card soft">
-			<div class="display-flex">
+			<div class="display-flex flex-wrap">
 				<div class="flex-stretch display-flex">
 			<div class="flex-none">
 				<button data-filter="category" data-value="" type="button" class="form-button">All</button>
